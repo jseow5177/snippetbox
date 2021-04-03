26fb69c402e55f96bb2680e09f21682ebeda35ff
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-03 15:40:28 +0800

    Use centralised error helper methods

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Check if the current request URL path exactly matches "/". If it doesn't, use
 	// the http.NotFound() function to send a 404 response to the client.
 	// Importantly, we then return from the handler. If we don't return the handler
 	// would keep executing and also write the "Hello from SnippetBox" message.
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -32,8 +31,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// to the user. Note that we can pass the slice of file paths as a variadic parameter.
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 
@@ -42,34 +40,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// that we want to pass in, which for now we'll set as nil.
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	// Extract the vakue of id parameter from query string.
 	// Try to convert it to an integer using the strconv.Atoi() function.
 	// If it can't be converted to an integer, or the value if less than 1, return a
 	// 404 page not found response
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	// Use r.Method to check whether the request is using POST or not. Note that
 	// http.MethodPost is a constant equal to the string "POST"
 	if r.Method != http.MethodPost {
 		// Add an "Allow: POST" header.
 		// Must be before w.WriteHeader and w.Write
 		w.Header().Set("Allow", http.MethodPost)
-		w.WriteHeader(http.StatusMethodNotAllowed) // Give response a status code of 405 "Method Not Allowed"
-		w.Write([]byte("Method Not Allowed"))
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
