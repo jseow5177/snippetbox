b1ce28c78f419a77aaa86be99ef211e0a8859016
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-11 21:28:49 +0800

    Add code to fetch a snippet and top 10 latest snippets

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/jseow5177/snippetbox/pkg/models"
 )
@@ -20,7 +21,7 @@ type SnippetModel struct {
 // Insert a new snippet into the database
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 
-	// Write the SQL statement we want to execute.
+	// INSERT SQL statement.
 	// The ? character is used to indicate placeholder parameters.
 	// Using parameterized queries instead of string interpolation can help
 	// to prevent SQL injection.
@@ -54,10 +55,84 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 
 // Return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	// SELECT SQL statement.
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	// Use QueryRow() on the connection pool to execute the SQL statement, 
+	// passing in the id variables as the value of placeholder parameter. This
+	// returns a pointer to a sql.Row object which holds the result from the database.
+	row := m.DB.QueryRow(stmt, id)
+
+	// Initialize a pointer to a new, empty Snippet struct
+	s := new(models.Snippet)
+
+	// Use rows.Scan() to copy the values from each field in sql.Row to the
+  // corresponding field in the Snippet struct. Notice that the arguments
+  // to row.Scan are *pointers* to the place you want to copy the data into,
+  // and the number of arguments must be exactly the same as the number of
+  // columns returned by your statement.
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
 }
 
 // Return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([] *models.Snippet, error) {
-	return nil, nil
+	// SELECT SQL statement.
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+
+	// Use the Query() method on the connection pool to execute the SQL
+	// statement. This returns a sql.Rows resultset containing the query result.
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// Defer rows.Close() to ensure the sql.Rows resultset is always
+	// properly closed before the Latest() method returns.
+	// If a resultset remains open, the underlying data connection will remain open.
+	defer rows.Close()
+
+	// Initialize an empty slice to hold the models.Snippets objects.
+	snippets := []*models.Snippet{}
+	
+	// Use rows.Next to iterate through the rows in the resultset. This 
+	// prepares the *first* (and then each subsequent) row to be acted on by the
+	// rows.Scan() method.
+	// If iteration over all the rows complete, the resultset automatically closes
+	// and frees-up the underlying database connection.
+	for rows.Next() {
+		s := new(models.Snippet)
+
+		// Use rows.Scan() to copy the values from each field in the row to the new Snippet object
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		// Append to the list of snippets
+		snippets = append(snippets, s)
+	}
+
+	// rows.Next() either returns true on success, or false if there is no next result row
+	// or an error happened while preparing it.
+	// To distinguish between two cases of false, we need to explicitly check for errors by calling
+	// rows.Err(). This retrieves any errors that was encountered during the iteration.
+	// It is important to call this, do not assume that a successful iteration was completed
+	// over the whole resultset.
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
