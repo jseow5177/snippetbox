66719c36f8bc1c3a24c2a3a4f817ec48b5fe10de
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-15 20:54:26 +0800

    Add comments

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -7,7 +7,12 @@ import (
 
 var ErrNoRecord = errors.New("models: no matching record found")
 
-// Data schema of Snippet
+// Database model of Snippet.
+// Go doesn't do very well in managing NULL values in database records.
+// Go cannot convert NULL into a string.
+// To solve this, we can use sql.NullString instead (https://golang.org/pkg/database/sql/#NullString)
+// But, as a rule, try to avoid NULL values altogether. Use either NOT NULL contraints along with DEFAULT
+// values.
 type Snippet struct {
 	ID int
 	Title string
