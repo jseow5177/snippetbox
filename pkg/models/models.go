39bd382414aa1560adad6d5b548282cbddd725bb
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-21 11:29:15 +0800

    Add User struct along with new errors

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,16 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+// Go handles the conversion of data types from SQL to native Go types.
+// Source code: https://golang.org/src/database/sql/driver/types.go
+
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	// Return this error if a user tries to login with an incorrect email or password.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	// Return this error if a user tries to signup with an email address that is already in use.
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
 
 // Database model of Snippet.
 // Go doesn't do very well in managing NULL values in database records.
@@ -19,4 +28,15 @@ type Snippet struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+
+// Database model of User
+type User struct {
+	ID int
+	Name string
+	Email string
+	HashedPassword []byte
+	Created time.Time
+	Active bool
 }
