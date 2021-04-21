ef31757ad1c2680261237ff54af7bf00162108f2
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-21 23:07:07 +0800

    Add user logout handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -187,5 +187,9 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Logout the user..."))
+	// Remove the authenticatedUserID from the session data so that the user is logged out
+	app.session.Remove(r, "authenticatedUserID")
+	// Add a Flash message to the session to confirm to the user that they've been logged out
+	app.session.Put(r, "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
