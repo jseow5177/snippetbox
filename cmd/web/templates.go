940f3d259b5f17020abc86f21f6a09fdc5186432
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-22 12:01:58 +0800

    Improve formatDate to handle empty time. Also convert to UTC before formatting

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -26,7 +26,13 @@ type templateData struct {
 // formatDate() is a custom template function that returns a nicely formatted
 // string representation of a time.Time object
 func formatDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	// Return the empty string if time has zero value
+	if t.IsZero() {
+		return ""
+	}
+
+	// Convert the time to UTC before formatting
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 // Initialize a template.FuncMap object and store it in a global variable. This is essentially
