592719b3a711bb9dd0915b2dbab24f250f978eb1
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-22 09:25:42 +0800

    Make logout and snippet creation route to require authentication

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -66,15 +66,15 @@ func (app *application) routes() http.Handler {
 	// Pat doesn't allow handler functions to be registered. Hence, they need to be
 	// converted using the http.HandlerFunc() adapter.
 	mux.Get("/", dynamicMiddleware.ThenFunc(app.home)) // Match requests where the URL path is exactly "/"
-	mux.Get("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm))
-	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
+	mux.Get("/snippet/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createSnippet))
 	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 
 	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
 	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
 	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
-	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser))
 
 	// A custom file system that disables directory listing
 	customFs := neuteredFileSystem {
