6a6e4038a437ce3e875a354ab6c789e86cb573d7
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-04-22 10:12:22 +0800

    Add CSRF token in templateData

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 // The serverError helper writes an error message and stack trace to the errorLog,
@@ -53,6 +55,9 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 
 	// Add the authentication status to the template data
 	td.IsAuthenticated = app.isAuthenticated(r)
+
+	// Add the CSRF token to the templateData struct
+	td.CSRFToken = nosurf.Token(r)
 
 	return td
 }
